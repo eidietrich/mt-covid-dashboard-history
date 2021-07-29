2d2ea448d144e71ecf1567982084004a0000ae39
eidietrich <eric.dietrich@gmail.com>
2021-07-29 13:01:06 -0600

    Add JS processing step

---
+++ process.js
@@ -0,0 +1,37 @@
+const fs = require('fs')
+
+const getJson = (path) => JSON.parse(fs.readFileSync(path))
+
+const writeJson = (path, data) => {
+    fs.writeFile(path, JSON.stringify(data), function (err) {
+        if (err) throw err;
+        console.log('Written to', path);
+    }
+    );
+}
+
+
+const full = getJson('full.json')
+
+const data = full.integrated_county_latest_external_data
+
+const mtData = data.filter(d => d['State_name'] === 'Montana')
+
+const keepKeys = [
+    'County',
+    'fips_code',
+    'community_transmission_level'
+]
+
+const mtDataCleaned = mtData.map(d => {
+    const output = {}
+    keepKeys.forEach(key => {
+        output[key] = d[key]
+    })
+    return output
+})
+
+writeJson('./montana-full.json', mtData)
+writeJson('./montana-cleaned.json', mtDataCleaned)
+
+// console.log(mtData.length)
