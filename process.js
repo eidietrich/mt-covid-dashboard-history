d59410eabc5e49b3b0fea28390e858f6a0367c08
eidietrich <eric.dietrich@gmail.com>
2022-04-13 10:52:10 -0600

    Add summary process step

--- process.js
+++ process.js
@@ -3,37 +3,62 @@ const fs = require('fs')
 const getJson = (path) => JSON.parse(fs.readFileSync(path))
 
 const writeJson = (path, data) => {
-    fs.writeFile(path, JSON.stringify(data), function (err) {
+    fs.writeFile(path, JSON.stringify(data, null, 2), function (err) {
         if (err) throw err;
         console.log('Written to', path);
     }
     );
 }
 
+const byCountyCases = getJson('mt-county-case-snapshot.json')
 
-const full = getJson('full.json')
+if (byCountyCases.features && byCountyCases.features.length === 56) {
+    const data = byCountyCases.features.map(d => d.attributes)
 
-const data = full.integrated_county_latest_external_data
+    const totalDeaths = data.reduce((acc, obj) => obj.TotalDeaths + acc, 0)
+    const cumulativeCases = data.reduce((acc, obj) => obj.Total + acc, 0)
+    const totalActiveCases = data.reduce((acc, obj) => obj.TotalActive + acc, 0)
+    const totalNewCases = data.reduce((acc, obj) => obj.NewCases + acc, 0)
 
-if (data.length > 0) {
-    // conditional here keeps existing data for null data result
-    const mtData = data.filter(d => d['State_name'] === 'Montana')
+    const summary = {
+        'CountiesInData': byCountyCases.features.length,
+        totalDeaths,
+        cumulativeCases,
+        totalActiveCases,
+        totalNewCases,
+    }
+    writeJson('./dashboard-total-summary.json', summary)
 
-    const keepKeys = [
-        'County',
-        'fips_code',
-        'community_transmission_level'
-    ]
-
-    const mtDataCleaned = mtData.map(d => {
-        const output = {}
-        keepKeys.forEach(key => {
-            output[key] = d[key]
-        })
-        return output
-    })
-
-    writeJson('./montana-full.json', mtData)
-    writeJson('./montana-cleaned.json', mtDataCleaned)
-
+} else {
+    throw 'Not 56 counties in data'
 }
+
+
+
+// Old, unimplemented processing script
+// const full = getJson('full.json')
+
+// const data = full.integrated_county_latest_external_data
+
+// if (data.length > 0) {
+//     // conditional here keeps existing data for null data result
+//     const mtData = data.filter(d => d['State_name'] === 'Montana')
+
+//     const keepKeys = [
+//         'County',
+//         'fips_code',
+//         'community_transmission_level'
+//     ]
+
+//     const mtDataCleaned = mtData.map(d => {
+//         const output = {}
+//         keepKeys.forEach(key => {
+//             output[key] = d[key]
+//         })
+//         return output
+//     })
+
+//     writeJson('./montana-full.json', mtData)
+//     writeJson('./montana-cleaned.json', mtDataCleaned)
+
+// }
